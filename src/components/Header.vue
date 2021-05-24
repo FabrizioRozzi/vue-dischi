4104776f1443bc71252b202d795b0bccda4b6507
FabrizioRozzi <rozzi458@gmail.com>
2021-05-24 16:49:13 +0200

    bloccato

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="wrap d-flex align-items-center">
+    <img src="@/assets/img/logo.png" alt="logo">
+  </div>
+</template>
+
+<script>
+export default {
+  name : 'Header'
+}
+</script>
+
+<style lang="scss" scoped>
+
+  .wrap{
+    height: 100px;
+    background-color: #2E3A46;
+    img{
+      height: 80px;
+      background-color: #2E3A46;
+      padding-left: 10px;
+    }
+  }
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="total">
+    <div class="wrapper">
+      ciao
+    
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name : 'Main',
+  components:{
+ 
+  }
+  
+}
+</script>
+
+<style lang="scss" scoped>
+
+  .total{
+    width: 100%;
+    height: calc(100vh - 100px);
+    background-color: #1E2D3B;
+    .wrapper{
+      width: 60%;
+      height: 100%;
+      margin: 0 auto;
+      //background da cancellare
+      background-color: whitesmoke;
+    }
+  }
+
+</style>
